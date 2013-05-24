cb28e8ea0da06f1e90b25eca846819ab8d18b263
John B <john.b30@gmail.com>
2013-05-24 12:52:33 -0500

    Restructure and add main()

--- petrarch/petrarch.py
+++ petrarch/petrarch.py
@@ -1,6 +1,5 @@
 import nltk as nk
-import preprocess
-import itertools
+import parse
 import argparse
 from joblib import Parallel, delayed
 
@@ -41,43 +40,6 @@ def create_chunkers():
     return ubt_chunker
 
 
-class TagChunker(nk.chunk.ChunkParserI):
-    """Class used to create a chunker for use in parsing POS-tagged
-    sentences."""
-    def __init__(self, chunk_tagger):
-        self._chunk_tagger = chunk_tagger
-
-    def parse(self, tokens):
-        """
-        Function to chunk a POS-tagged sentence using a trained chunker.
-
-        Parameters
-        ----------
-
-        tokens: List.
-                POS-tagged sentence.
-
-        Returns
-        -------
-
-        chunked: NLTK tree object.
-                 Chunked sentence in NLTK tree form.
-
-        """
-        # split words and part of speech tags
-        (words, tags) = zip(*tokens)
-        # get IOB chunk tags
-        chunks = self._chunk_tagger.tag(tags)
-        # join words with chunk tags
-        wtc = itertools.izip(words, chunks)
-        # w = word, t = part-of-speech tag, c = chunk tag
-        lines = [' '.join([w, t, c]) for (w, (t, c)) in wtc if c]
-        # create tree from conll formatted chunk lines
-        chunked = nk.chunk.conllstr2tree('\n'.join(lines))
-
-        return chunked
-
-
 def read_data(filepath):
     """
     Function to read in a file of news stories. Expects stories formatted
@@ -107,189 +69,6 @@ def read_data(filepath):
         event_dict[ident] = story_info
 
     return event_dict
-
-
-def _get_np(tree):
-    """
-    Private function to extract noun phrases from a parse tree of any given
-    sentence.
-
-    Parameters
-    ----------
-
-    tree: NLTK tree object.
-          Parse tree for a particular sentence.
-
-    Returns
-    -------
-
-    noun_phrases: List.
-                  Noun phrases within a sentence.
-    """
-    noun_phrases = []
-    for sub in tree.subtrees(filter=lambda x: x.node == 'NP'):
-        nouns = []
-        for i in xrange(len(sub)):
-            nouns.append(sub[i][0])
-        nouns = ' '.join(nouns)
-        noun_phrases.append(nouns)
-
-    return noun_phrases
-
-
-def _get_vp(tree):
-    """
-    Private function to extract verb phrases from a parse tree of any given
-    sentence.
-
-    Parameters
-    ----------
-
-    tree: NLTK tree object.
-          Parse tree for a particular sentence.
-
-    Returns
-    -------
-
-    verb_phrases: List of tuples.
-                  Collection of verb phrases in a sentence. The tuples are
-                  of the form (verb, noun phrase), where the
-                  noun phrase is one that immediately follows the verb.
-
-    """
-    verb_phrases = []
-    prev_type = None
-    prev_chunk = None
-    for sub in tree.subtrees(filter=lambda x: x != 'S'):
-        if prev_type == 'VP' and sub.node == 'NP':
-            verb = ' '.join([vw[0] for vw in prev_chunk])
-            noun = ' '.join([nw[0] for nw in sub])
-            verb_phrases.append((verb, noun))
-        prev_type = sub.node
-        prev_chunk = sub
-
-    return verb_phrases
-
-
-def parse_sent(sent, key, input_chunker):
-    """
-    Function to parse a given sentence. Tokenizes, POS tags, and chunks a
-    given sentence, along with extracting noun and verb phrases.
-
-    Parameters
-    ----------
-
-    sent: String.
-          Sentence to be parsed.
-
-    key: String.
-         Key of the event used to access the event from the events
-         dictionary.
-
-    input_chunker: trained NLTK chunker.
-
-    Returns
-    -------
-
-    sub_event_dict: Dictionary.
-                    Container for  the POS-tagged sentence, chunked
-                    sentence, noun phrases, and verb phrases for each event
-                    in the input file.
-
-    """
-    sub_event_dict = {key: {}}
-    #Tokenize the words
-    toks = nk.word_tokenize(sent)
-    #Part-of-speech tag the tokens
-    tags = nk.pos_tag(toks)
-    tags = [tag for tag in tags if tag[1] != 'POS']
-    chunker = TagChunker(input_chunker)
-    #Use chunker to chunk the tagged words and combine into a tree
-    tree = chunker.parse(tags)
-    sub_event_dict[key]['tagged'] = tags
-    sub_event_dict[key]['sent_tree'] = tree
-
-    noun_phrases = _get_np(tree)
-    verb_phrases = _get_vp(tree)
-
-    sub_event_dict[key]['noun_phrases'] = noun_phrases
-    sub_event_dict[key]['verb_phrases'] = verb_phrases
-
-    return sub_event_dict
-
-
-def post_process(sent, key, username):
-    """
-    Helper function to call the various post-processing functions, e.g.
-    geolocation and feature extraction.
-
-    Parameters
-    ----------
-
-    sent: String.
-          Sentence to parse.
-
-    key: String.
-         Key of the sentence in the general events dictionary.
-
-    Username: String.
-              Geonames username.
-
-    """
-    sub_event_dict = {key: {}}
-    #Tokenize the words
-    toks = nk.word_tokenize(sent)
-    #Part-of-speech tag the tokens
-    tags = nk.pos_tag(toks)
-    pp = preprocess.Process(tags)
-    lat, lon = pp.geolocate(username)
-    sub_event_dict[key]['lat'] = lat
-    sub_event_dict[key]['lon'] = lon
-
-    sub_event_dict[key]['num_involved'] = pp.num_involved()
-
-    return sub_event_dict
-
-
-def parse(event_dict, input_chunker, username, process2=False):
-    """
-    Function that calls the `parse_sent` function in parallel. Helper
-    function to make calling the necessary functions cleaner.
-
-    Parameters
-    ------
-
-    event_dict: Dictionary.
-                Dictionary containing the sentences to be parsed. Assumes
-                each event has a unique identifier as the key.
-
-    input_chunker: trained NLTK chunker.
-
-    Note
-    ----
-
-    This is the function that should be called directly in a script.
-
-    """
-    parsed = Parallel(n_jobs=-1)(delayed(parse_sent)
-                                 (sent=event_dict[key]['story'], key=key,
-                                  input_chunker=input_chunker)
-                                 for key in event_dict)
-
-    for parsed_sent in parsed:
-        key = parsed_sent.keys()[0]
-        event_dict[key].update(parsed_sent[key])
-
-    if process2:
-        post_parsed = Parallel(n_jobs=-1)(delayed(post_process)
-                                         (sent=event_dict[key]['story'],
-                                          key=key,
-                                          username=username)
-                                          for key in event_dict)
-
-        for post_parsed_sent in post_parsed:
-            key = post_parsed_sent.keys()[0]
-            event_dict[key].update(post_parsed_sent[key])
 
 
 def parse_cli_args():
@@ -336,32 +115,40 @@ PETRARCH
     args = aparse.parse_args()
     return args
 
-if __name__ == '__main__':
+
+def main():
+    """Main function"""
     cli_args = parse_cli_args()
     cli_command = cli_args.command_name
     inputs = cli_args.inputs
     out_path = cli_args.output
     username = cli_args.username
     post_proc = cli_args.postprocess
+    if cli_command == 'parse':
+        print 'Reading in data...'
+        ubt_chunker = create_chunkers()
+        print 'Parsing sentences...'
+        events = read_data(inputs)
+        parse.parse(events, ubt_chunker, username, post_proc)
+        for event in events:
+            print '=======================\n'
+            print 'event id: {}\n'.format(event)
+            print 'POS tagged sent:\n {}\n'.format(events[event]['tagged'])
+            print 'NP tagged sent:\n {}\n'.format(events[event]['sent_tree'])
+            print 'Noun phrases: \n {}\n'.format(events[event]['noun_phrases'])
+            print 'Verb phrases: \n {}\n'.format(events[event]['verb_phrases'])
+            try:
+                print 'Geolocate: \n {}, {}\n'.format(events[event]['lat'],
+                                                      events[event]['lon'])
+            except KeyError:
+                pass
+            try:
+                print 'Feature extract: \n {}\n'.format(events[event]['num_involved'])
+            except KeyError:
+                pass
+    else:
+        print 'Please enter a valid command!'
 
-    print 'Reading in data...'
-    ubt_chunker = create_chunkers()
-    print 'Parsing sentences...'
-    events = read_data(inputs)
-    parse(events, ubt_chunker, username, post_proc)
-    for event in events:
-        print '=======================\n'
-        print 'event id: {}\n'.format(event)
-        print 'POS tagged sent:\n {}\n'.format(events[event]['tagged'])
-        print 'NP tagged sent:\n {}\n'.format(events[event]['sent_tree'])
-        print 'Noun phrases: \n {}\n'.format(events[event]['noun_phrases'])
-        print 'Verb phrases: \n {}\n'.format(events[event]['verb_phrases'])
-        try:
-            print 'Geolocate: \n {}, {}\n'.format(events[event]['lat'],
-                                                  events[event]['lon'])
-        except KeyError:
-            pass
-        try:
-            print 'Feature extract: \n {}\n'.format(events[event]['num_involved'])
-        except KeyError:
-            pass
+
+if __name__ == '__main__':
+    main()
